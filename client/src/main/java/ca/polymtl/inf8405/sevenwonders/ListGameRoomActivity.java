234f0045b647dcc2aa87963a3848df3c5eb18beb
honguj <hoangtayducnguyen@gmail.com>
2013-04-10 13:47:51 -0400

    Merge GeolocateActivity to ListGameRoomActivity. The GeolocateActivity is used for Guillaume testing purpose only

--- client/src/main/java/ca/polymtl/inf8405/sevenwonders/ListGameRoomActivity.java
+++ client/src/main/java/ca/polymtl/inf8405/sevenwonders/ListGameRoomActivity.java
@@ -3,8 +3,11 @@ package ca.polymtl.inf8405.sevenwonders;
 import java.util.ArrayList;
 
 import android.app.Activity;
-import android.app.ListActivity;
-import android.content.Intent;
+import android.content.Context;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
@@ -14,42 +17,81 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
- 
-public class ListGameRoomActivity extends Activity {
- 
-	private ArrayAdapter adapter;
-	static String[] FRUITS = new String[] { "Apple", "Avocado", "Banana",
-			"Blueberry", "Coconut", "Durian", "Guava", "Kiwifruit",
-			"Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple",
-			"Jackfruit", "Mango", "Olive", "Pear", "Sugar-apple"};
- 
+
+public class ListGameRoomActivity extends Activity implements LocationListener{
+
+	private static ArrayAdapter adapter_;
 	static ArrayList<String> ROOMS = new ArrayList<String>();
-	
+	private LocationManager locationManager_;
+	/// Temporary String
+	private String locationString_ = "";
+
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.activity_game_rooms);
- 
+
+		// Create a list view of game room
 		final ListView listView = (ListView) findViewById(R.id.listView);
-		adapter = new ArrayAdapter<String>(this, R.layout.gameroom_item,ROOMS);
-		listView.setAdapter(adapter);
- 
+		adapter_ = new ArrayAdapter<String>(this, R.layout.gameroom_item,ROOMS);
+		listView.setAdapter(adapter_);
 		listView.setTextFilterEnabled(true);
- 
 		listView.setOnItemClickListener(new OnItemClickListener() {
 			public void onItemClick(AdapterView<?> parent, View view,
 					int position, long id) {
-			    // When clicked, show a toast with the TextView text
-			    Toast.makeText(getApplicationContext(),
-				((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+				// TODO: Join game room
+				Toast.makeText(getApplicationContext(),
+						((TextView) view).getText(), Toast.LENGTH_SHORT).show();
 			}
 		});
+
+		// Create location manager 
+		locationManager_ = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		// Define the criteria how to select the locatioin provider -> use
+		// default
+		Criteria criteria = new Criteria();
+		String provider = locationManager_.getBestProvider(criteria, false);
+		Location location = locationManager_.getLastKnownLocation(provider);
+		if (location != null) {
+			onLocationChanged(location);
+		} else {
+			locationString_ = "Location not available";
+		}
 	}
-	
+
 	public void createGame(View view){
+		// TODO: Send request Create (locationString) to server and wait
 		int random = 1000 + (int)(Math.random() * ((9999 - 1000) + 1));
-		ROOMS.add("Room " + random);
-		adapter.notifyDataSetChanged();
+		ROOMS.add("Room " + random + " - " + locationString_);
+
+		adapter_.notifyDataSetChanged();
+	}
+
+	//////////////////////////// Location Listener ///////////////////////////////////// 
+	@Override
+	public void onLocationChanged(Location location) {
+		// TODO Auto-generated method stub
+		locationString_ = "Current location = " +
+				String.valueOf(location.getLatitude()) + " ; " +
+				String.valueOf(location.getLongitude());
+	}
+
+	@Override
+	public void onProviderDisabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onProviderEnabled(String arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onStatusChanged(String arg0, int arg1, Bundle arg2) {
+		// TODO Auto-generated method stub
+
 	}
 }
